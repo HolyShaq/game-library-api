9ef9e69abd5929f43fd766b37f1a98b5652f4d2f
HolyShaq <acebyronehalili@gmail.com>
2025-07-25 13:53:44 +0800

    chore: extract port config to env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 
@@ -5,7 +7,8 @@ app.get("/", (req, res) => {
   res.send("Hello Worlds!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-  console.log("http://localhost:3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
