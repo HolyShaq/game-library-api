a53f4c8c0fda4d6df1c9b9dbb667194c81e9c715
HolyShaq <acebyronehalili@gmail.com>
2025-07-26 18:29:13 +0800

    feat: add log middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,10 +1,37 @@
-import { writeLog } from "../utils/logger.js";
+import chalk from "chalk";
+import { writeLog, calculateDuration } from "../utils/logger.js";
 
 export const logger = (req, res, next) => {
-  next()
+  const start = process.hrtime();
 
-  console.log("Logged")
-  writeLog("Logged!!!")
-}
+  res.on("finish", () => {
+    const timestamp = `[${new Date().toISOString()}]`;
+    const method = req.method;
+    const url = req.originalUrl;
+    const status = res.statusCode;
+    const duration = `(${calculateDuration(start)} ms)`;
 
-export default logger
+    let color;
+    let level;
+    if (status >= 500) {
+      color = chalk.red;
+      level = "ERROR";
+    } else if (status >= 400) {
+      color = chalk.yellow;
+      level = "WARN";
+    } else {
+      color = chalk.green;
+      level = "INFO";
+    }
+
+    const rawLog = `${timestamp} ${level}: ${method} ${url} ${status} ${duration}`;
+    const coloredLog = `${chalk.grey(timestamp)} ${color(level)}: ${method} ${url} ${color(status)} ${chalk.grey(duration)}`;
+
+    writeLog(rawLog);
+    console.log(coloredLog);
+  });
+
+  next();
+};
+
+export default logger;
--- utils/logger.js
+++ utils/logger.js
@@ -5,7 +5,16 @@ import path from "path";
 const logDir = path.resolve("logs");
 if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
 
+// Function that writes to log file
 export const writeLog = (log) => {
   const logPath = path.join(logDir, "logs.log");
-  fs.appendFileSync(logPath, log + "\n", "utf8")
+  fs.appendFileSync(logPath, log + "\n", "utf8");
+};
+
+// Helper function for calculating process duration
+export const calculateDuration = (start) => {
+  const [sec, nano] = process.hrtime(start);
+  const duration = (sec * 1e3 + nano / 1e6).toFixed(2); // ms
+
+  return duration
 };
